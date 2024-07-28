f42aeb7557f0bf6589042969825b0fb0ad0872b5
UllasFPLE <ullasjs@fple.in>
2024-07-28 12:04:58 +0530

    Added modal employee create started

---
+++ pages/projects/projects-list/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="content">
+        Project List Page
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'app',
+    middleware: ['auth']
+})
+const userStore = useUserStore()
+</script>
--- pages/staff/employees/index.vue
+++ pages/staff/employees/index.vue
@@ -1,10 +1,21 @@
 <template>
     <div class="content">
-        Employees
+        <div class="flex justify-between items-center">
+            <div>Employees</div>
+            <div>
+                <NuxtLink class="btn btn-add" to="employees/create">Add</NuxtLink>
+            </div>
+        </div>
+        <div class="box mt-5">
+            Filter part
+        </div>
     </div>
 </template>
 
 <script setup>
+useSeoMeta({
+    title: 'Employees',
+})
 definePageMeta({
     layout: 'app',
     middleware: ['auth']
